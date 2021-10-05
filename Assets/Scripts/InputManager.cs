3fc7f245e8cd25e46746b8de5693d9652a9590dc
Ssamior <59698224+Ssamior@users.noreply.github.com>
2021-10-05 17:39:19 +0200

    Seance 3
    Double saut
    Dash avec animation
    Sprint
    Reset de saut sur les murs
    Plateformes mobiles

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,6 +7,12 @@ public class InputManager : MonoBehaviour
     public float acceleration;
     public float verticalacceleration;
     public float jumpHeight;
+    public float dashCooldown;
+    public float baseAirFriction;
+    public float fallingAirFriction;
+    public float baseGroundFriction;
+    public float sprintGroundFriction;
+    public LayerMask Walls;
 
     private Vector3 jumpStartPosition;
     private GameObject ground;
@@ -14,6 +20,11 @@ public class InputManager : MonoBehaviour
     private Vector3 moveDirection;
     private Vector3 positionInit;
     private Vector3 positionFin;
+    private int jumpsNumber;
+    private float horizontalInput;
+    private float lastDash;
+    private float groundFriction;
+    private float airFriction;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +32,9 @@ public class InputManager : MonoBehaviour
         positionFin = positionInit;
         ground = GameObject.Find("Ground");
         jumpStartPosition = positionInit;
+        jumpsNumber = 0;
+        airFriction = baseAirFriction;
+        groundFriction = baseGroundFriction;
     }
 
 
@@ -34,16 +48,35 @@ public class InputManager : MonoBehaviour
     {
         positionInit = transform.position;
         positionFin = positionInit;
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        //Inputs
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Dash();
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("shift down");
+            groundFriction = sprintGroundFriction;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Debug.Log("shift up");
+            groundFriction = baseGroundFriction;
+        }
 
         //Deplacement horizontal
-        moveDirection = Input.GetAxis("Horizontal") * acceleration * Vector3.right;
+        moveDirection = horizontalInput * acceleration * groundFriction * airFriction * Vector3.right;
         positionFin += moveDirection;
 
         //Saut
-        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space) &&!isJumpingUp)
+        if (CanJump())
         {
             jumpStartPosition = positionInit;
             isJumpingUp = true;
+            jumpsNumber += 1;
         }
         //En train de sauter
         if(isJumpingUp)
@@ -60,18 +93,32 @@ public class InputManager : MonoBehaviour
         //Chute
         if(!IsGrounded() && !isJumpingUp)
         {
+            airFriction = fallingAirFriction;
             positionFin += Vector3.down * verticalacceleration * Time.deltaTime;
+        }
+
+        //Cooldowns
+        if (Time.time - lastDash <= dashCooldown)
+        {
+            GetComponent<SpriteRenderer>().color = Color.Lerp(Color.black, Color.red, (Time.time - lastDash)/dashCooldown);
         }
 
 
         //Collisions
         if(IsBlockedRight())
         {
+            jumpsNumber = 1;
             positionFin.x = Mathf.Min(positionInit.x,positionFin.x);
         }
         if (IsBlockedLeft())
         {
+            jumpsNumber = 1;
             positionFin.x = Mathf.Max(positionInit.x, positionFin.x);
+        }
+        if (IsGrounded())
+        {
+            jumpsNumber = 0;
+            airFriction = baseAirFriction;
         }
 
         //Mouvement
@@ -83,16 +130,32 @@ public class InputManager : MonoBehaviour
     //TODO : dimensions relatives au Player
     bool IsGrounded() 
     {
-        return Physics2D.Raycast(transform.position - 0.51f * Vector3.up, Vector3.down, 0.01f);
+        return Physics2D.Raycast(transform.position - transform.localScale.y / 2 * Vector3.up, Vector3.down, 0.01f, Walls) && !isJumpingUp;
     }
 
     bool IsBlockedRight()
     {
-        return Physics2D.Raycast(transform.position + 0.51f * Vector3.right, Vector3.right, 0.01f);
+        return Physics2D.Raycast(transform.position + transform.localScale.x / 2 * Vector3.right, Vector3.right, 0.01f, Walls);
     }
 
     bool IsBlockedLeft()
     {
-        return Physics2D.Raycast(transform.position + 0.51f * Vector3.left, Vector3.left, 0.01f);
+        return Physics2D.Raycast(transform.position + transform.localScale.x / 2 * Vector3.left, Vector3.left, 0.01f, Walls);
+    }
+    bool CanJump()
+    {
+        return (jumpsNumber < 2 && Input.GetKeyDown(KeyCode.Space));
+    }
+
+    void Dash()
+    {
+        Vector3 dashPosition = positionInit + 3 * Mathf.Sign(horizontalInput) * Vector3.right;
+        //Si le chemin est libre
+        if (!Physics2D.Raycast(positionInit, Mathf.Sign(horizontalInput) * Vector3.right, Vector3.Distance(positionInit, dashPosition), Walls) && (Time.time - lastDash) > dashCooldown)
+        {
+            positionFin = dashPosition;
+            lastDash = Time.time;
+        }
+
     }
 }
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float platformSpeed;
+    public float distance;
+    [Tooltip("True = moving horizontally / False = moving vertically")]
+    public bool horizontalPlatform;
+
+    private bool returnToPosition;
+    private Vector3 positionInit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionInit = transform.position;
+        returnToPosition = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Deplacement
+        if (horizontalPlatform)
+        {
+            if (returnToPosition)
+            {
+                transform.position = transform.position + Vector3.right * Time.deltaTime * platformSpeed;
+            }
+            else
+            {
+                transform.position = transform.position - Vector3.right * Time.deltaTime * platformSpeed;
+            }
+        }
+        else
+        {
+            if (returnToPosition)
+            {
+                transform.position = transform.position + Vector3.up * Time.deltaTime * platformSpeed;
+            }
+            else
+            {
+                transform.position = transform.position - Vector3.up * Time.deltaTime * platformSpeed;
+            }
+        }
+
+        //Inversion du sens
+        if (Vector3.Distance(positionInit,transform.position) >= distance)
+        {
+            returnToPosition = !returnToPosition;
+        }
+    }
+}
